ad0bff08bb0f0bb8ea2f49efedabad78c8747be2
GrProgrammer <joepetrosyan@mail.ru>
2017-09-17 23:05:48 +0400

    developers images

---
+++ untitled1/index.js
@@ -0,0 +1,43 @@
+window.addEventListener("scroll", parallax);
+function parallax() {
+    var headSection = document.getElementsByTagName("header")[0];
+    headSection.style.backgroundPosition = "center " + -(window.pageYOffset/5) + "px";
+}
+
+var d = document.getElementById("parallax");
+
+var options = {
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+var map = new google.maps.Map(d, options);
+
+function success(position) {
+    var loc = new google.maps.LatLng(position.coords.latitude,
+        position.coords.longitude);
+    map.setCenter(loc);
+    var label = new google.maps.InfoWindow();
+    label.setContent("You are here!");
+    label.setPosition(loc);
+    label.open(map);
+}
+
+function error(err) {
+    var s = "";
+    switch(err){
+        case 1: s = "No permission";break;
+        case 2: s = "Technical error";break;
+        case 3: s = "Timeout";break;
+        default: s = "Unknown error";
+    }
+    d.innerHTML = s;
+}
+
+params = {
+    timeout: 10000
+};
+
+navigator
+    .geolocation
+    .getCurrentPosition(success, error, params);
