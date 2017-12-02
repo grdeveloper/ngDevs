4ab505dc2f0b86f5f67bd1fb72704c9501a90916
GrProgrammer <joepetrosyan@mail.ru>
2017-12-02 05:34:09 +0400

    Add files via upload

---
+++ common.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $(".imgFolder").magnificPopup({
+        type : "image",
+        gallery: {
+            enabled : true
+        },
+        removalDelay : 300,
+        mainClass : 'mfp-fade'
+    });
+
+});
